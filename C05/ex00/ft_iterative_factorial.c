8271b343d51950a1639e2f2cf23534c598948755
Ax0ou <axel.alvarade@hotmail.fr>
2025-09-06 14:18:20 +0200

    Add files via upload

---
+++ C05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalvarad <aalvarad.studient.42lausanne.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/02 10:12:52 by aalvarad          #+#    #+#             */
+/*   Updated: 2025/09/03 08:24:27 by aalvarad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	i;
+	int	result;
+
+	i = 1;
+	result = 1;
+	if (nb < 0)
+	{
+		return (0);
+	}
+	while (i <= nb)
+	{
+		result *= i++;
+	}
+	return (result);
+}
